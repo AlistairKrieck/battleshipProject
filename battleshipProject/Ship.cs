55780622e9cfed85210e6e3ddd02e31cb5b1d5b8
aliskrie701 <aliskrie701@ed.amdsb.ca>
2025-03-07 14:55:51 -0500

    Created ship class

---
+++ battleshipProject/Ship.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleshipProject
+{
+    public class Ship
+    {
+        public Tile tile;
+        public string name;
+        public int xSize, ySize;
+
+        public Ship(Tile _tile, string _name, int _xSize, int _ySize)
+        {
+            tile = _tile;
+            name = _name;
+            xSize = _xSize;
+            ySize = _ySize;
+        }
+
+        public partial class LittleGuy : Ship
+        {
+            Tile _tile = new Tile();
+            string _name = "littleGuy";
+            int _xSize = 2;
+            int _ySize = 1;
+        }
+    }
+}
